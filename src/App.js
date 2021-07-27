11a36c3b4433bac0e94aeb5617c8080b80e8045e
viktoryka <mikisum321@gmail.com>
2021-07-27 20:26:50 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Dialogs from './components/Navbar/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/MyPosts/MyPosts';
+import News from './components/Navbar/News/News';
+import Music from './components/Navbar/Music/Music';
+import Settings from './components/Navbar/Settings/Settings';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />  
-      <Navbar />
-      <Profile />
-    </div>);
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />  
+        <Navbar />
+        {/* <Profile /> */}
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes  from './Profile.module.css'
 
 const Profile = () => {
     return (
-    <div className={classes.content}>
+    <div>
       <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'></img>
       <div>
         ava + description
