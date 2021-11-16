8029030bc01dcc0e9cfd1d868c28afe01ca5b700
viktoryka <mikisum321@gmail.com>
2021-11-16 13:09:10 +0300

    feat: apply ant-design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ComponentType, FC, useEffect } from 'react';
-import { BrowserRouter, Route, withRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, withRouter, Switch, Link } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -16,19 +16,18 @@ import store, { AppStateType } from './redux/redux-store'
 import Friends from './components/Friends/Friends';
 import FriendsContainer from './components/Friends/FriendsContainer';
 import { QueryParamProvider } from 'use-query-params';
-import Header from './components/Header/Header';
-// import ProfileContainer from './components/Profile/ProfileContainer';
+import {Header} from './components/Header/Header';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, HomeOutlined,
+  MessageOutlined, TeamOutlined, WechatOutlined  } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+
+const { SubMenu } = Menu;
+const { Content, Footer, Sider } = Layout;
 
 const DialogsContainer = React.lazy(() => import('./components/Navbar/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
 const ChatPage = React.lazy(() => import('./pages/Chat/ChatPage'))
-
-type MapStatePropsType = ReturnType<typeof mapStateToProps>
-
-type MapDispatchPropsType = {
-  initializeApp: () => void
-}
-
 
 const App: FC = () => {
   const authorizedUserId = useSelector((state:AppStateType) => state.auth.userId)
@@ -42,46 +41,59 @@ const App: FC = () => {
     }
   },[initialized])
 
-  // catchAllUnhandledErrors = (e: PromiseRejectionEvent) => {
-  //   alert(PromiseRejectionEvent)
-  // }
-
-  // componentDidMount () {
-  //   this.props.initializeApp()
-  //   window.addEventListener('unhandledrejection', this.catchAllUnhandledErrors)
-  // }
-  
-  // componentWillUnmount() {
-  //   window.removeEventListener('unhandledrejection', this.catchAllUnhandledErrors)
-  // }
-
-
     if (initialized) {
       return <Preloader/>
     }
 
     return (
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />  
-        <div className='app-wrapper-content'>
-        <React.Suspense fallback={<Preloader />}>
-          <Switch>
-            <Redirect exact from='/' to='/profile'/>
-            <Route path='/login' render={() => <LoginPage />} />
-            <Route path='/dialogs' render={() => <DialogsContainer/>} />
-            <Route path='/profile/:userId?' render={() =><ProfileContainer/>} />
-            <Route path='/users' render={() => <UsersPage pageTitle='Social Network'/>} />
-            <Route path='/chat' render={() => <ChatPage/>} />
-            <Route path='/news' render={News} />
-            <Route path='/music' render={Music} />
-            <Route path='/settings' render={Settings} />
-            <Route path='*' render={() => <div>404 NOT FOUND</div>} />
-          </Switch>
-        </React.Suspense>
-        </div>
-        <FriendsContainer />
-      </div>
+      <Layout>
+      <Header/>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              style={{ height: '100%' }}
+            >
+              <Menu.Item key='1' icon={<UserOutlined/>}> 
+                <Link to={`/profile/`}>Profile</Link>
+              </Menu.Item>
+              <Menu.Item key='2' icon={<MessageOutlined />}>
+                <Link to="/dialogs">Messages</Link>
+              </Menu.Item>
+              <Menu.Item key='3' icon={<TeamOutlined />}>          
+                <Link to="/users">Users</Link>
+              </Menu.Item>
+              <Menu.Item key='4' icon={<WechatOutlined />}>          
+                <Link to="/chat">Chat</Link>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          <React.Suspense fallback={<Preloader />}>
+           <Switch>
+             <Redirect exact from='/' to='/profile'/>
+             <Route path='/login' render={() => <LoginPage />} />
+             <Route path='/dialogs' render={() => <DialogsContainer/>} />
+             <Route path='/profile/:userId?' render={() =><ProfileContainer/>} />
+             <Route path='/users' render={() => <UsersPage pageTitle='Social Network'/>} />
+             <Route path='/chat' render={() => <ChatPage/>} />
+             <Route path='/news' render={News} />
+             <Route path='/music' render={Music} />
+             <Route path='/settings' render={Settings} />
+             <Route path='*' render={() => <div>404 NOT FOUND</div>} />
+           </Switch>
+         </React.Suspense>
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
     )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,51 @@
+import { Avatar, Button, Layout } from 'antd'
 import React, { FC } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { logout } from '../../redux/auth-reducer'
 import { AppStateType } from '../../redux/redux-store'
 import classes from './Header.module.css'
+import { Row, Col, Divider, Menu } from 'antd';
+import {UserOutlined, HomeOutlined} from '@ant-design/icons'
 
-const Header: FC= () => {
+export const Header: FC= () => {
 
   const isAuth = useSelector((state: AppStateType) => state.auth.isAuth)
-  const authorizedUserId = useSelector((state:AppStateType) => state.auth.userId)
+  const profile = useSelector((state: AppStateType) => state.profilePage.profile)
 
   const login = useSelector((state: AppStateType) => state.auth.login)
-  // const login = JSON.parse(localStorage.data).login
-
-  // const login = localStorage.getItem('login')
   const dispatch = useDispatch()
+  const {Header} = Layout
 
+  const logoutCallback = () => {
+    dispatch(logout())
+  }
   return (
-    <header className={classes.header}>
-      <div>
-        <img src='https://ak.picdn.net/shutterstock/videos/19546093/thumb/1.jpg' alt=''></img>
-      </div>
-      <div className={classes.loginBlock}>
-          {isAuth 
-          ? <div>{login} - <button onClick={() => dispatch(logout())}>Log out</button></div>
-          : <NavLink to={'/login'}>Login</NavLink>}
-      </div>
-    </header>
+    <Header className='header'>
+      <Row>
+      <Col span={21}>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1"><Link to="/profile"><HomeOutlined /></Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/users">Developers</Link></Menu.Item>
+        </Menu>
+      </Col>
+
+      {isAuth 
+      ? <><Col span={1}> 
+            <Avatar 
+              src={profile?.photos.small}
+              alt={login || ''} style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}/>
+          </Col>
+          <Col span={2}>
+            <Button onClick={logoutCallback}>Log out</Button>
+          </Col>
+        </>
+      : <Col>
+          <Button>
+            <Link to={'/login'}>Login</Link>
+          </Button>
+        </Col>}
+      </Row>
+    </Header>
   )
 }
-
-export default Header
